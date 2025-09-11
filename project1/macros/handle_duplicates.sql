05bb45d6f49aafdc7071a63c68b93c87db3b2fb6
Vinayak-Sitaram Parab <vinayak.parab@qbeyond-ds.de>
2025-09-11 15:53:09 +0200

    Added new macros as per the test cases to handle  duplicates and business keys

---
+++ project1/macros/handle_duplicates.sql
@@ -0,0 +1,25 @@
+{% macro flag_duplicates(source_relation, key_column, attribute_columns, timestamp_column='edwLoadDate') %}
+
+WITH flagged AS (
+    SELECT *,
+        COUNT(*) OVER (PARTITION BY {{ key_column }}) AS cnt,
+        COUNT(DISTINCT {{ attribute_columns | join(" || ") }}) 
+            OVER (PARTITION BY {{ key_column }}) AS distinct_attr_count,
+        ROW_NUMBER() OVER (PARTITION BY {{ key_column }} ORDER BY {{ timestamp_column }} DESC) AS rn
+    FROM {{ source_relation }}
+),
+
+cleaned AS (
+    SELECT *,
+        CASE
+            WHEN cnt > 1 AND distinct_attr_count = 1 THEN 'actual_duplicate'
+            WHEN cnt > 1 AND distinct_attr_count > 1 THEN 'contradictory_duplicate'
+            ELSE 'unique'
+        END AS duplicate_type
+    FROM flagged
+)
+
+SELECT *
+FROM cleaned
+
+{% endmacro %}
